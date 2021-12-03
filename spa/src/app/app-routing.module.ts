48a4ee951a7f257570ade93c351105f841d2ee7a
Venugopal K <venu.shastri@gmail.com>
2021-12-03 09:29:45 +0530

    Add files via upload

---
+++ spa/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  {path:"",redirectTo:"home",pathMatch:"full"},
+  {path:"home" , component:HomeComponent,
+  children:[
+    {path:'login',component:LoginComponent},
+    {path:'signup',component:SignupComponent}
+  ]
+  },
+  {path:"dashboard",component:DashboardComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
